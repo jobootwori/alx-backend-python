a7d299ec7969a36566248eea612ea5f2353f98fe
Joshua Otwori <jobootwori@gmail.com>
2023-01-09 21:07:08 +0300

    [UPDATE]: Initial changes

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
-
+'''Task 0's module.
+'''
 import asyncio
 import random
 
+
 async def wait_random(max_delay=10):
+    '''Waits for a random number of seconds.
+    '''
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
